4183eaae121a6c9bcc4d9aecba94b885fa6b9877
dagimgetaw <dagimgetaw27@gmail.com>
2026-07-18 22:28:11 +0300

    feat: add homepage messaging to introduce atronsa’s vision

---
+++ landing/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center px-8 bg-zinc-50 dark:bg-black font-montserrat">
+      <p className="text-xs md:text-sm lg:text-base font-medium text-center leading-relaxed">
+        Building secure, offline-first, and accessible digital payments for
+        everyone.
+      </p>
+      <p className="text-[9px] md:text-[10px] lg:text-xs pt-3 font-medium text-stone-400">
+        Comming Soon.
+      </p>
+    </div>
+  );
+}
