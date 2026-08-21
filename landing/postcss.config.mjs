730ab28079c6a4b2cc8c3debaf14bbda7572441e
dagimgetaw <dagimgetaw27@gmail.com>
2026-08-21 09:31:08 +0300

    feat: add PostCSS configuration

---
+++ landing/postcss.config.mjs
@@ -0,0 +1,22 @@
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          cssnano: {
+            preset: [
+              "default",
+              {
+                discardComments: { removeAll: true },
+                normalizeWhitespace: true,
+                minifyFontValues: true,
+                minifyGradients: true,
+              },
+            ],
+          },
+        }
+      : {}),
+  },
+};
+
+export default config;
